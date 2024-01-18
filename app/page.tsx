cd6699ea97e10901a7b14b2328b77261367f65cb
Tylah Kapa <tylahkapa@gmail.com>
2024-01-18 10:19:32 +1000

    Update with page

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-3 w-full">
-        <div className="flex flex-col w-full text-center border rounded-md p-4 gap-4">
-          <h2>Angular</h2>
-          <Link
-            href={"/quiz/angular"}
-            className="p-2 border w-fit self-center hover:cursor-pointer"
-          >
-            Take Quiz
-          </Link>
-        </div>
-        <div className="flex flex-col w-full text-center border rounded-md p-4">
-          Angular
-          <button className="p-2 border w-fit">Take Quiz</button>
-        </div>
-        <div className="flex flex-col w-full text-center border rounded-md p-4">
-          Angular
-          <button className="p-2 border w-fit">Take Quiz</button>
-        </div>
+    <div className="flex gap-3 w-full">
+      <div className="flex flex-col w-full text-center border rounded-md p-4 gap-4">
+        <h2>Angular</h2>
+        <Link
+          href={"/quiz/angular"}
+          className="p-2 border w-fit self-center hover:cursor-pointer"
+        >
+          Take Quiz
+        </Link>
       </div>
-    </main>
+      <div className="flex flex-col w-full text-center border rounded-md p-4">
+        Angular
+        <button className="p-2 border w-fit">Take Quiz</button>
+      </div>
+      <div className="flex flex-col w-full text-center border rounded-md p-4">
+        Angular
+        <button className="p-2 border w-fit">Take Quiz</button>
+      </div>
+    </div>
   );
 }
--- app/quiz/[slug]/page.tsx
+++ app/quiz/[slug]/page.tsx
@@ -1,12 +1,113 @@
+"use client";
 import React from "react";
 
-export async function getStaticProps() {}
+const AngularQuestions = [
+  "What is Angular?",
+  "What is Angular CLI?",
+  "What is Angular Material?",
+];
+
+interface QuizState {
+  currentQuestion: number;
+  questions: string[];
+  responses: any[];
+  correct: number;
+}
 
 const Quiz = ({ params }: { params: { slug: string } }) => {
+  const initialState: QuizState = {
+    currentQuestion: 0,
+    questions: AngularQuestions,
+    responses: [],
+    correct: 0,
+  };
+
+  const [currentState, setCurrentState] =
+    React.useState<QuizState>(initialState);
+
+  const [answerText, setAnswerText] = React.useState<string>("");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [quizComplete, setQuizComplete] = React.useState<boolean>(false);
+
+  const sendAnswer = async () => {
+    const { responses, currentQuestion, questions } = currentState;
+
+    const newResponses = responses;
+
+    newResponses.push({
+      question: questions[currentQuestion],
+      answer: answerText,
+      correct: true,
+      explanation: "This is a placeholder explanation",
+      confidence: 0,
+    });
+
+    var nextQuestion = currentQuestion + 1;
+
+    if (currentQuestion === questions.length - 1) {
+      setQuizComplete(true);
+      nextQuestion = currentQuestion;
+    }
+
+    setCurrentState({
+      ...currentState,
+      responses: newResponses,
+      currentQuestion: nextQuestion,
+    });
+
+    setAnswerText("");
+  };
+
   return (
-    <main>
-      <h1>Quiz for {params.slug}</h1>
-    </main>
+    <div className="flex w-full h-full gap-10 justify-between">
+      <section className="flex flex-col w-full h-full gap-4">
+        <h1>Quiz for {params.slug.toUpperCase()}</h1>
+        <div className="flex flex-col border p-4 rounded-md gap-4 h-52">
+          {!quizComplete ? (
+            <>
+              <span className="w-full flex">
+                Question {currentState.currentQuestion + 1} of{" "}
+                {currentState.questions.length}
+              </span>
+              <label htmlFor="answer-box">
+                <h2>{currentState.questions[currentState.currentQuestion]}</h2>
+              </label>
+              <input
+                name="answer-box"
+                type="text"
+                className="h-full text-black p-4"
+                value={answerText}
+                onChange={(e) => {
+                  setAnswerText(e.target.value);
+                }}
+              />
+              <button onClick={sendAnswer}>Submit Answer</button>
+            </>
+          ) : (
+            <>
+              <h2>Quiz Complete!</h2>
+              <span>Score: {currentState.correct}</span>
+            </>
+          )}
+        </div>
+      </section>
+
+      <section className="w-full h-1/2 flex flex-col gap-4">
+        <h1>Responses</h1>
+        <div className="flex flex-col h-full divide-y-2 gap-4 bg-slate-900 rounded-md px-4 pb-4">
+          {currentState.responses.map((response, i) => {
+            return (
+              <div className="flex flex-col gap-4 pt-4" key={i}>
+                <h2>{response.question}</h2>
+                <span>Your Answer: {response.answer}</span>
+                <span>Explanation: {response.explanation}</span>
+                <span>Confidence: {response.confidence}</span>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 };
 
